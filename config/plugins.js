e4031b006c5b76a3e5ee4e55e6c5c27d02397425
kokaipatrik <steveepatrik@gmail.com>
2025-10-30 15:13:38 +0100

    feat: add '@strapi/plugin-graphql'

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      landingPage: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
+});
